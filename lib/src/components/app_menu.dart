1da280de44f4a254c3193437f827afd3365dec44
Charles <charleslanop@gmail.com>
2021-07-13 21:22:53 -0300

    feat(menu): update

--- lib/src/components/app_menu.dart
+++ lib/src/components/app_menu.dart
@@ -14,7 +14,7 @@ class AppMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height / 7.5;
+    final height = MediaQuery.of(context).size.height / 9.8;
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 5),
       child: ElevatedButton(
